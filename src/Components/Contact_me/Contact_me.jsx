f109746079cf62760d7fe43566abdb9100245e08
el-nazgul-cansado <francojoaquinbeccari@gmail.com>
2025-07-21 23:20:13 -0300

    Creado componente para contact_me, se intena subir a github pages, se cambia la carpeta raiz

---
+++ src/Components/Contact_me/Contact_me.jsx
@@ -0,0 +1,21 @@
+import './Contact_me.css'
+
+export const Contact_me = () => {
+
+    return(
+        <div>
+            <div>
+            <a href="">whatsapp</a>
+            <a href="">linkedin</a>
+            <a href="">facebook</a>
+            </div>
+            <div>
+                <input type="text" />
+                <input type="email" />
+                <input type="text" />
+                <input type="text" />
+                <input type="submit" />
+            </div>
+        </div>
+    )
+}
